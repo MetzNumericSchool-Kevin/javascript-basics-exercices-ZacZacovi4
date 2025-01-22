b854594b0c70f4918b8eb36509622efaebf90381
KUKOBA Vadym <zaczacovi4@gmail.com>
2025-01-22 09:38:47 +0100

    exo 2 Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -2,3 +2,9 @@ const nom_boutique = "BomBoClatz magie";
 let nb_potion = 15864;
 let prix_potion = 18;
 let overture = true;
+
+if (overture === true) {
+    console.log("Bienvenue dans la boutique " + nom_boutique + " Aventurier ! 🎉");
+}else{
+    console.log("La boutique " + nom_boutique + " est fermée, revenez plus tard Aventurier ! 😴");
+}
